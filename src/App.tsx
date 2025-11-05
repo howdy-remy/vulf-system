d3488a979a74f8e4d8b9ab17d594eb28886d0a18
Remy Lynch <howdyremy@gmail.com>
2025-11-04 19:37:41 -0500

    section header

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Header } from "./components/Header";
 import { AlbumCard } from "./components/Album";
 
 import { albums } from "./data/albums";
+import { SectionHeader } from "./components/SectionHeader";
 
 function App() {
   return (
     <>
       <Header />
+      <SectionHeader title="discography" />
       {albums.map((album) => (
         <AlbumCard key={album.id} album={album} />
       ))}
